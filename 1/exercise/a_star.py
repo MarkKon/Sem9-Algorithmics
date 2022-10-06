b34c9be62e203b4f87c3c94a0cc0a82b6e942514
MARKKon <konstantin.mark99@gmail.com>
2022-10-06 05:46:53 +0200

    Remove unnecessary code.

--- 1/exercise/a_star.py
+++ 1/exercise/a_star.py
@@ -29,25 +29,15 @@ class DirectedGraph(object):
     return None
   
   def astar(self, start_node, end_node, heuristic):
-    # The open set is the set of nodes we have discovered but not yet
-    # visited.  It is a priority queue, with the lowest cost node at the
-    # top.
+    # Undiscovered nodes as priority queue
     open_set = PriorityQueue()
     open_set.put(start_node, 0)
 
-    # The closed set is the set of nodes we have visited.
-    closed_set = set()
-
-    # The came_from dictionary is used to reconstruct the path at the end.
-    came_from = {}
-
-    # The g_score dictionary stores the cost of the cheapest path from
-    # the start node to a given node.
+    # g_score
     g_score = {node : np.inf for node in self.index_set}
     g_score[start_node] = 0
 
-    # The f_score dictionary stores the cost of the cheapest path from
-    # the start node to the goal by passing by a given node.
+    # f_score
     f_score = {}
     f_score[start_node] = heuristic[start_node]
 
@@ -61,16 +51,11 @@ class DirectedGraph(object):
         print("Final f_score dictionary", f_score)
         return True
 
-      # closed_set.add(current)
-
       for neighbor in self.neighbors(current):
-        # if neighbor in closed_set:
-        #   continue
 
         tentative_g_score = g_score[current] + self.cost(current, neighbor)
 
         if tentative_g_score < g_score[neighbor]:
-          came_from[neighbor] = current
           print("coming from", current, "to", neighbor)
           g_score[neighbor] = tentative_g_score
           f_score[neighbor] = tentative_g_score + heuristic[neighbor]
